8a78ad6428e22f1075bdc927937951e62531efa9
Niket Dave <ndave@codal.com>
2021-03-13 00:47:01 +0530

    Added files and changes required for deployment to heroku

--- crudexample/settings.py
+++ crudexample/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
